d61e90e14f51ada8283d0ac299c93420120dde53
Michael Munje <michaelmunje@gmail.com>
2019-02-21 14:07:34 -0800

    Add additional tests.

--- test/Spec.hs
+++ test/Spec.hs
@@ -15,6 +15,16 @@ spec = do
             parseIExp "1+1" `shouldBe` (Right $ IExp (IExpInt 1) Plus (IExpInt 1))
             parseIExp "x-1" `shouldBe` (Right $ IExp (IExpVar $ Identifier "x") Minus (IExpInt 1))
             parseIExp "x*y" `shouldBe` (Right $ IExp (IExpVar $ Identifier "x") Mult (IExpVar $ Identifier "y"))
+            parseIExp "x/1" `shouldBe` (Right $ IExp (IExpVar $ Identifier "x") Div (IExpInt 1))
+            parseIExp "x" `shouldBe` (Right $ IExpVar $ Identifier "x")
+            parseIExp "1" `shouldBe` (Right $ IExpInt 1)
+            parseIExp "2==y" `shouldBe` (Right $ IExp (IExpInt 2) Equals (IExpVar $ Identifier "y"))
+            parseIExp "2^y" `shouldBe` (Right $ IExp (IExpInt 2) Exponent (IExpVar $ Identifier "y"))
+            -- parseIExp "\02^y" `shouldBe` (Left "failed" Message)
+
+            -- parseIExp "x/0" `shouldBe` (Right $ IExp (IExpVar $ Identifier "x") Div (IExpInt 0)) -- should be changed to error later
+            -- parseIExp "2*x+y" `shouldBe` (Right $ IExp (IExp (IExpInt 2) Mult (IExpVar $ Identifier "x")) Plus (IExpVar $ Identifier "y"))
+
             -- "x*y==z" unresolved bug with recursive integer expressions
     
     describe "expressions" $ do
@@ -25,9 +35,12 @@ spec = do
             parseExp "\"xyz\"" `shouldBe` (Right $ ExpString "xyz")
             parseExp "\\(x){x}:String" `shouldBe` (Right (ExpLambda (ExpVariable $ Identifier "x") (ExpVariable $ Identifier "x") (Type $ Identifier "String")))
             parseExp "name(x)" `shouldBe` (Right (ExpFOCall (Identifier "name") (ExpVariable $ Identifier "x")))
-    
+            -- parseExp "name()" `shouldBe` (Right (ExpFOCall (Identifier "name") (ExpVariable $ Identifier "x")))
+
     describe "top level function and data definitions" $ do
         it "parses tlds" $ do
             parseTld "datanewType=Nullary" `shouldBe` (Right (DataDef (Identifier "newType") [NullaryConstructor $ Identifier "Nullary"]))
             -- unresolved bug with multiple constructors
             parseTld "funk=func(a:string):string{a}" `shouldBe` (Right (FuncDef (Identifier "funk") (Identifier "a") (Type $ Identifier "string") (ExpVariable $ Identifier "a") (Type $ Identifier "string")))
+            -- parseTld "funk=func():string{a}" `shouldBe` (Right (FuncDef (Identifier "funk") (ExpVariable $ Identifier "a") (Type $ Identifier "string")))
+
