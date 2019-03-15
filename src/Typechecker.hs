b7cba86208b2ae43d9b428f8555b62c7e929569b
Shant Hairapetian <shant.hairapetian@metalab.csun.edu>
2019-03-15 16:32:30 -0700

    typechecking variables

--- src/Typechecker.hs
+++ src/Typechecker.hs
@@ -14,19 +14,19 @@ data Gamma = Gamma [(Identifier, Type)] deriving (Show)
 addEntry :: Identifier -> Type -> Gamma -> Gamma
 addEntry n t (Gamma l) = Gamma (l ++ [(n,t)])
 
-getType :: Gamma -> Identifier -> Maybe Type
-getType (Gamma g) x = let gMap = fromList g in do
+getType :: Identifier -> Gamma -> Maybe Type
+getType x (Gamma g) = let gMap = fromList g in do
     t <- lookup x gMap
     return t
 
 class Typecheck a where
     typecheck :: a -> State Gamma (Maybe Type)
-    
+
 
 instance Typecheck Tld where
     typecheck (FuncDefUnary fName cName inType body outType) = do
         actualOutType <- typecheck body
-        case actualOutType == (Just outType) of
+        case actualOutType == Just outType of
             True -> do
                 gamma <- get
                 _ <- put $ addEntry fName functionType gamma
@@ -34,20 +34,33 @@ instance Typecheck Tld where
                 where
                     functionType = FunctionType inType  outType
             False -> return Nothing
+    
+    typecheck (FuncDefNullary fName body t) = do
+        actualType <- typecheck body
+        case actualType == Just t of
+            True -> do
+                gamma <- get
+                _ <- put $ addEntry fName t gamma
+                return $ Just t
+            False -> return Nothing
+    
 
 
 instance Typecheck IExp where
     typecheck (IExpVar id) = do
         gamma <- get
-        case getType gamma id of
-            Just (Type (Identifier t)) -> return (Just $ Type $ Identifier $ t)
+        case getType id gamma of
+            Just (Type (Identifier t)) -> return (Just $ mkType $ t)
             Nothing -> return Nothing
-    typecheck iExp = return (Just $ Type $ Identifier $ "Int")
+    typecheck iExp = return (Just $ mkType $ "Int")
 
 instance Typecheck Exp where
-    typecheck (ExpInteger ei) = return $ Just $ Type $ Identifier "Int"
-    typecheck (ExpString es) = return $ Just $ Type $ Identifier "String"
+    typecheck (ExpInteger ei) = return $ Just $ mkType "Int"
+    typecheck (ExpString es) = return $ Just $ mkType "String"
     typecheck (ExpIExp eie) = typecheck eie
+    typecheck (ExpVariable x) = do
+        gamma <- get
+        return $ getType x gamma
     typecheck (ExpLambda e1 t1 e2 t2) = do
         e1t <- typecheck e1
         e2t <- typecheck e2
@@ -58,4 +71,4 @@ instance Typecheck Exp where
 main = runState (typecheck x) gamma
             where 
                 x = IExpVar (Identifier "var")
-                gamma = Gamma [(Identifier "var", Type $ Identifier "string")]
+                gamma = Gamma [(Identifier "var", mkType "string")]
