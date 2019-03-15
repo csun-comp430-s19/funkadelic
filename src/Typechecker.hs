5b41a4b2efec77479ababf5e5382c3b62df1e81e
Shant Hairapetian <shant.hairapetian@metalab.csun.edu>
2019-03-15 12:33:42 -0700

    POC

--- src/Typechecker.hs
+++ src/Typechecker.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE FlexibleContexts #-}
 module Typechecker where
 
+import Control.Monad.State.Lazy
 import Prelude hiding (lookup)
 import Parser
 import Data.Map
 
 -- Type environment
-data Gamma = Gamma [(Identifier, Type)]
+data Gamma = Gamma [(Identifier, Type)] deriving (Show)
+
+
 
 getType :: Gamma -> Identifier -> Maybe Type
 getType (Gamma g) x = let gMap = fromList g in do
@@ -14,20 +17,30 @@ getType (Gamma g) x = let gMap = fromList g in do
     return t
 
 class Typecheck a where
-    typecheck :: a -> Maybe Type
+    typecheck :: a -> State Gamma String
 
 instance Typecheck IExp where
-    typecheck iExp = Just $ Type $ Identifier "Int"
+    typecheck (IExpVar id) = do
+        gamma <- get
+        case getType gamma id of
+            Just (Type (Identifier t)) -> return t
+            Nothing -> return "undeclared variable"
+    typecheck iExp = return "Int"
 
-instance Typecheck Exp where
-    typecheck (ExpInteger ei) = Just $ Type $ Identifier "Int"
-    typecheck (ExpString es) = Just $ Type $ Identifier "String"
-    typecheck (ExpIExp eie) 
-        | typecheck eie == Just (Type $ Identifier "Int") = Just $ Type $ Identifier "Int"
-        | otherwise = Nothing
-    typecheck (ExpLambda e1 t1 e2 t2)
-        | paramTypeStatus && returnTypeStatus = Just t2
-        | otherwise = Nothing
-        where 
-            paramTypeStatus = typecheck e1 == Just t1
-            returnTypeStatus = typecheck e2 == Just t2
+main = runState (typecheck x) gamma
+            where 
+                x = IExpVar (Identifier "var")
+                gamma = Gamma [(Identifier "var", Type $ Identifier "string")]
+
+-- instance Typecheck Exp where
+--     typecheck (ExpInteger ei) = Just $ Type $ Identifier "Int"
+--     typecheck (ExpString es) = Just $ Type $ Identifier "String"
+--     typecheck (ExpIExp eie) 
+--         | typecheck eie == Just (Type $ Identifier "Int") = Just $ Type $ Identifier "Int"
+--         | otherwise = Nothing
+--     typecheck (ExpLambda e1 t1 e2 t2)
+--         | paramTypeStatus && returnTypeStatus = Just t2
+--         | otherwise = Nothing
+--         where 
+--             paramTypeStatus = typecheck e1 == Just t1
+--             returnTypeStatus = typecheck e2 == Just t2
