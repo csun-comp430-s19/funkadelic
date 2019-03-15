cc154c1c7bae2256d09c0fc43ff9a717ed3ac27d
Shant Hairapetian <shant.hairapetian@metalab.csun.edu>
2019-03-15 13:04:12 -0700

    working

--- src/Typechecker.hs
+++ src/Typechecker.hs
@@ -2,6 +2,8 @@
 module Typechecker where
 
 import Control.Monad.State.Lazy
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Maybe
 import Prelude hiding (lookup)
 import Parser
 import Data.Map
@@ -9,38 +11,34 @@ import Data.Map
 -- Type environment
 data Gamma = Gamma [(Identifier, Type)] deriving (Show)
 
-
-
 getType :: Gamma -> Identifier -> Maybe Type
 getType (Gamma g) x = let gMap = fromList g in do
     t <- lookup x gMap
     return t
 
 class Typecheck a where
-    typecheck :: a -> State Gamma String
+    typecheck :: a -> State Gamma (Maybe Type)
 
 instance Typecheck IExp where
     typecheck (IExpVar id) = do
         gamma <- get
         case getType gamma id of
-            Just (Type (Identifier t)) -> return t
-            Nothing -> return "undeclared variable"
-    typecheck iExp = return "Int"
+            Just (Type (Identifier t)) -> return (Just $ Type $ Identifier $ t)
+            Nothing -> return Nothing
+    typecheck iExp = return (Just $ Type $ Identifier $ "Int")
 
 main = runState (typecheck x) gamma
             where 
                 x = IExpVar (Identifier "var")
                 gamma = Gamma [(Identifier "var", Type $ Identifier "string")]
 
--- instance Typecheck Exp where
---     typecheck (ExpInteger ei) = Just $ Type $ Identifier "Int"
---     typecheck (ExpString es) = Just $ Type $ Identifier "String"
---     typecheck (ExpIExp eie) 
---         | typecheck eie == Just (Type $ Identifier "Int") = Just $ Type $ Identifier "Int"
---         | otherwise = Nothing
---     typecheck (ExpLambda e1 t1 e2 t2)
---         | paramTypeStatus && returnTypeStatus = Just t2
---         | otherwise = Nothing
---         where 
---             paramTypeStatus = typecheck e1 == Just t1
---             returnTypeStatus = typecheck e2 == Just t2
+instance Typecheck Exp where
+    typecheck (ExpInteger ei) = return $ Just $ Type $ Identifier "Int"
+    typecheck (ExpString es) = return $ Just $ Type $ Identifier "String"
+    typecheck (ExpIExp eie) = typecheck eie
+    typecheck (ExpLambda e1 t1 e2 t2) = do
+        e1t <- typecheck e1
+        e2t <- typecheck e2
+        case e1t == (Just t1) && e2t == (Just t2) of
+            True -> return (Just t1)
+            False -> return Nothing
