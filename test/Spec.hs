bd0cf29d6fbad22f4cb6a5af28a7f47ba0cabc54
Michael Munje <michaelmunje@gmail.com>
2019-03-15 22:53:19 -0700

    Add few more bad input tests for mismatched types.

--- test/Spec.hs
+++ test/Spec.hs
@@ -68,8 +68,10 @@ spec = do
         it "typechecks integer expressions" $ do
             let typeEnv = (Gamma [(Identifier "x", mkType "Int")])
             (evalState (typecheck (IExpVar (Identifier "x"))) typeEnv) `shouldBe` intType
+
             let typeEnv = (Gamma [(Identifier "x", mkType "Int"), (Identifier "y", mkType "String"), (Identifier "z", mkType "Int")])
             (evalState (typecheck (IExpVar (Identifier "y"))) typeEnv) `shouldBe` stringType
+
             (evalState (typecheck (IExp (IExpInt 1) Plus (IExpInt 1))) typeEnv) `shouldBe` intType
             (evalState (typecheck (IExpInt 1)) typeEnv) `shouldBe` intType
 
@@ -78,19 +80,34 @@ spec = do
         it "typechecks Expressions" $ do
             let typeEnv = (Gamma [(Identifier "x", mkType "Int")])
             (evalState (typecheck (ExpInteger 1234)) typeEnv) `shouldBe` intType
+
             (evalState (typecheck (ExpString "xyz")) typeEnv) `shouldBe` stringType
+
             let typeEnv = (Gamma [(Identifier "x", mkType "Int"), (Identifier "y", mkType "String"), (Identifier "z", mkType "Int")])
             (evalState (typecheck (ExpIExp (IExpVar (Identifier "y")))) typeEnv) `shouldBe` stringType
+
             (evalState (typecheck (ExpIExp (IExpVar (Identifier "z")))) typeEnv) `shouldBe` intType
             (evalState (typecheck (ExpIExp (IExpInt 1))) typeEnv) `shouldBe` intType
             (evalState (typecheck (ExpIExp (IExp (IExpInt 1) Plus (IExpInt 1)))) typeEnv) `shouldBe` intType
             (evalState (typecheck (ExpLambda (ExpInteger 1234) (type' "Int") (ExpInteger 1234) (type' "Int"))) typeEnv) `shouldBe` intType
             (evalState (typecheck (ExpLambda (ExpString "1234") (type' "String") (ExpInteger 1234) (type' "Int"))) typeEnv) `shouldBe` intType
             (evalState (typecheck (ExpLambda (ExpInteger 1234) (type' "Int") (ExpString "1234") (type' "String"))) typeEnv) `shouldBe` stringType
+            (evalState (typecheck (ExpLambda (ExpInteger 1234) (type' "Int") (ExpInteger 1234) (type' "String"))) typeEnv) `shouldBe` Nothing
+
             let typeEnv = (Gamma [(Identifier "x", mkType "Int"), (Identifier "y", mkType "String"), (Identifier "name", (mkFuncType "Int" "String"))])
             (evalState (typecheck (ExpUnaryFOCall (Identifier "name") (ExpVariable $ Identifier "x"))) typeEnv) `shouldBe` stringType
+
+            let typeEnv = (Gamma [(Identifier "x", mkType "Int"), (Identifier "y", mkType "String"), (Identifier "name", (mkFuncType "String" "String"))])
+            (evalState (typecheck (ExpUnaryFOCall (Identifier "name") (ExpVariable $ Identifier "x"))) typeEnv) `shouldBe` Nothing
+
+            let typeEnv = (Gamma [(Identifier "x", mkType "Int"), (Identifier "y", mkType "String")])
+            (evalState (typecheck (ExpUnaryFOCall (Identifier "name") (ExpVariable $ Identifier "x"))) typeEnv) `shouldBe` Nothing
+
             let typeEnv = (Gamma [(Identifier "x", mkType "Int"), (Identifier "y", mkType "String"), (Identifier "name", mkType "Int")])
             (evalState (typecheck (ExpNullaryFOCall (Identifier "name"))) typeEnv) `shouldBe` intType
+
+            let typeEnv = (Gamma [(Identifier "x", mkType "Int")])
+            (evalState (typecheck (ExpNullaryFOCall (Identifier "name"))) typeEnv) `shouldBe` Nothing
 
 
     describe "typechecking tlds" $ do
