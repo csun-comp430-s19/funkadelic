469b664ff0cdb71d675609b38c916d4a89d9c814
Michael Munje <michaelmunje@gmail.com>
2019-03-28 12:07:16 -0700

    Generalize parseAndTypecheck function. Apply it in tests.

--- test/Integration/Spec.hs
+++ test/Integration/Spec.hs
@@ -2,6 +2,7 @@
 import Parser hiding (type')
 import Typechecker
 import Test.Hspec
+import Control.Monad.Fail
 import Control.Monad.State.Lazy
 
 main :: IO ()
@@ -16,26 +17,10 @@ parseIExp input = parse' iExpParser input
 parseExp input = parse' expParser input
 parseTld input = parse' tldParser input
 
-parseIExpAndTypecheck :: String -> Gamma -> Maybe Type
-parseIExpAndTypecheck x y = do
-    parseResultWrapped <- return (parseIExp x)
-    Just parseResult <- return (getRight parseResultWrapped)
-    typeResult <- return (evalState (typecheck parseResult) y)
-    Just result <- return typeResult
-    return result
-
--- Can this be simplified into one expression?
-parseExpAndTypecheck :: String -> Gamma -> Maybe Type
-parseExpAndTypecheck x y = do
-    parseResultWrapped <- return (parseExp x)
-    Just parseResult <- return (getRight parseResultWrapped)
-    typeResult <- return (evalState (typecheck parseResult) y)
-    Just result <- return typeResult
-    return result
-
-parseTldAndTypecheck :: String -> Gamma -> Maybe Type
-parseTldAndTypecheck x y = do
-    parseResultWrapped <- return (parseTld x)
+parseAndTypecheck :: (Typecheck a1, Control.Monad.Fail.MonadFail m) =>
+     (t -> Either a2 a1) -> t -> Gamma -> m Type
+parseAndTypecheck f x y = do
+    parseResultWrapped <- return (f x)
     Just parseResult <- return (getRight parseResultWrapped)
     typeResult <- return (evalState (typecheck parseResult) y)
     Just result <- return typeResult
@@ -52,33 +37,33 @@ spec = do
      describe "integrate integer expressions with parser and typechecker" $ do
          it "PASSES on good parse input, good typechecker input" $ do
             let typeEnv = (Gamma [(Identifier "x", mkType "Int")])
-            (parseIExpAndTypecheck "1+1" typeEnv) `shouldBe` intType
+            (parseAndTypecheck parseIExp "1+1" typeEnv) `shouldBe` intType
 
             let typeEnv = (Gamma [(Identifier "x", mkType "Int")])
-            (parseIExpAndTypecheck "x" typeEnv) `shouldBe` intType
+            (parseAndTypecheck parseIExp "x" typeEnv) `shouldBe` intType
 
             let typeEnv = (Gamma [(Identifier "x", mkType "Int")])
-            (parseIExpAndTypecheck "1" typeEnv) `shouldBe` intType
+            (parseAndTypecheck parseIExp "1" typeEnv) `shouldBe` intType
 
-            (parseIExpAndTypecheck "x*1+2-3/4" typeEnv) `shouldBe` intType
+            (parseAndTypecheck parseIExp "x*1+2-3/4" typeEnv) `shouldBe` intType
 
             let typeEnv = (Gamma [(Identifier "x", mkType "Int"), (Identifier "y", mkType "String"), (Identifier "z", mkType "Int")])
-            (parseIExpAndTypecheck "y" typeEnv) `shouldBe` stringType
+            (parseAndTypecheck parseIExp "y" typeEnv) `shouldBe` stringType
 
          it "FAILS on bad parse input" $ do
             let typeEnv = (Gamma [(Identifier "x", mkType "Int")])
-            (parseIExpAndTypecheck "2*_" typeEnv) `shouldBe` Nothing
-            (parseIExpAndTypecheck "2*8a" typeEnv) `shouldBe` Nothing
-            (parseIExpAndTypecheck "3^+2" typeEnv) `shouldBe` Nothing
-            (parseIExpAndTypecheck "3^" typeEnv) `shouldBe` Nothing
-            (parseIExpAndTypecheck "x23232 + _2" typeEnv) `shouldBe` Nothing
-            (parseIExpAndTypecheck "_2+(2)" typeEnv) `shouldBe` Nothing
-            (parseIExpAndTypecheck "" typeEnv) `shouldBe` Nothing
-            (parseIExpAndTypecheck "\\(x):String{x}:String" typeEnv) `shouldBe` Nothing
+            (parseAndTypecheck parseIExp "2*_" typeEnv) `shouldBe` Nothing
+            (parseAndTypecheck parseIExp "2*8a" typeEnv) `shouldBe` Nothing
+            (parseAndTypecheck parseIExp "3^+2" typeEnv) `shouldBe` Nothing
+            (parseAndTypecheck parseIExp "3^" typeEnv) `shouldBe` Nothing
+            (parseAndTypecheck parseIExp "x23232 + _2" typeEnv) `shouldBe` Nothing
+            (parseAndTypecheck parseIExp "_2+(2)" typeEnv) `shouldBe` Nothing
+            (parseAndTypecheck parseIExp "" typeEnv) `shouldBe` Nothing
+            (parseAndTypecheck parseIExp "\\(x):String{x}:String" typeEnv) `shouldBe` Nothing
 
          -- it "FAILS on bad typechecker input" $ do
          --    let typeEnv = (Gamma [(Identifier "x", mkType "String")])
-            -- (parseIExpAndTypecheck "2*x" typeEnv) `shouldBe` Nothing
+            -- (parseAndTypecheck "2*x" typeEnv) `shouldBe` Nothing
             -- Above is a bug and should be nothing, since it is int * string
 
 
@@ -87,38 +72,38 @@ spec = do
      describe "integrate expressions with parser and typechecker" $ do
          it "PASSES on good parse input, good typechecker input" $ do
             let typeEnv = (Gamma [(Identifier "x", mkType "Int")])
-            (parseExpAndTypecheck "1234" typeEnv) `shouldBe` intType
-            (parseExpAndTypecheck "\"xyz\"" typeEnv) `shouldBe` stringType
+            (parseAndTypecheck parseExp "1234" typeEnv) `shouldBe` intType
+            (parseAndTypecheck parseExp "\"xyz\"" typeEnv) `shouldBe` stringType
             let typeEnv = (Gamma [(Identifier "x", mkType "Int"), (Identifier "y", mkType "String"), (Identifier "z", mkType "Int")])
-            (parseExpAndTypecheck "y" typeEnv) `shouldBe` stringType
+            (parseAndTypecheck parseExp "y" typeEnv) `shouldBe` stringType
             (evalState (typecheck (ExpIExp (IExpVar (Identifier "z")))) typeEnv) `shouldBe` intType
-            (parseExpAndTypecheck "1" typeEnv) `shouldBe` intType
-            (parseExpAndTypecheck "1+1" typeEnv) `shouldBe` intType
+            (parseAndTypecheck parseExp "1" typeEnv) `shouldBe` intType
+            (parseAndTypecheck parseExp "1+1" typeEnv) `shouldBe` intType
 
-            (parseExpAndTypecheck "\\(1234):Int{1234}:Int" typeEnv) `shouldBe` intType
-            (parseExpAndTypecheck "\\(1234):Int{\"1234\"}:String" typeEnv) `shouldBe` stringType
+            (parseAndTypecheck parseExp "\\(1234):Int{1234}:Int" typeEnv) `shouldBe` intType
+            (parseAndTypecheck parseExp "\\(1234):Int{\"1234\"}:String" typeEnv) `shouldBe` stringType
 
 
             let typeEnv = (Gamma [(Identifier "x", mkType "Int"), (Identifier "y", mkType "String"), (Identifier "name", (mkFuncType "Int" "String"))])
-            (parseExpAndTypecheck "name(x)" typeEnv) `shouldBe` stringType
+            (parseAndTypecheck parseExp "name(x)" typeEnv) `shouldBe` stringType
 
             let typeEnv = (Gamma [(Identifier "x", mkType "Int"), (Identifier "y", mkType "String"), (Identifier "name", mkType "Int")])
-            (parseExpAndTypecheck "name()" typeEnv) `shouldBe` intType
+            (parseAndTypecheck parseExp "name()" typeEnv) `shouldBe` intType
 
          it "FAILS on bad parse input" $ do
             let typeEnv = (Gamma [(Identifier "x", mkType "Int")])
-            (parseExpAndTypecheck "name(x" typeEnv) `shouldBe` Nothing
+            (parseAndTypecheck parseExp "name(x" typeEnv) `shouldBe` Nothing
 
          it "FAILS on bad typechecker input" $ do
             let typeEnv = (Gamma [(Identifier "x", mkType "Int")])
-            (parseExpAndTypecheck "name()" typeEnv) `shouldBe` Nothing
+            (parseAndTypecheck parseExp "name()" typeEnv) `shouldBe` Nothing
             let typeEnv = (Gamma [(Identifier "x", mkType "Int"), (Identifier "y", mkType "String"), (Identifier "name", (mkFuncType "String" "String"))])
-            (parseExpAndTypecheck "name(x)" typeEnv) `shouldBe` Nothing
+            (parseAndTypecheck parseExp "name(x)" typeEnv) `shouldBe` Nothing
             let typeEnv = (Gamma [(Identifier "x", mkType "Int"), (Identifier "y", mkType "String")])
-            (parseExpAndTypecheck "name(x)" typeEnv) `shouldBe` Nothing
-            (parseExpAndTypecheck "\\(1234):String{1234}:Int" typeEnv) `shouldBe` Nothing
-            (parseExpAndTypecheck "\\(1234):String{1234}:String" typeEnv) `shouldBe` Nothing
-            (parseExpAndTypecheck "\\(1234):Int{1234}:String" typeEnv) `shouldBe` Nothing
+            (parseAndTypecheck parseExp "name(x)" typeEnv) `shouldBe` Nothing
+            (parseAndTypecheck parseExp "\\(1234):String{1234}:Int" typeEnv) `shouldBe` Nothing
+            (parseAndTypecheck parseExp "\\(1234):String{1234}:String" typeEnv) `shouldBe` Nothing
+            (parseAndTypecheck parseExp "\\(1234):Int{1234}:String" typeEnv) `shouldBe` Nothing
 
 
 
@@ -126,18 +111,18 @@ spec = do
          it "PASSES on good parse input, good typechecker input" $ do
 
             let typeEnv = (Gamma [(Identifier "funk", mkType "String")])
-            (parseTldAndTypecheck "funk=func(a:String):String{a}" typeEnv) `shouldBe` Just (FunctionType (Type (Identifier "String")) (Type (Identifier "String")))
+            (parseAndTypecheck parseTld "funk=func(a:String):String{a}" typeEnv) `shouldBe` Just (FunctionType (Type (Identifier "String")) (Type (Identifier "String")))
 
             let typeEnv = (Gamma [(Identifier "anotherFunk", mkType "Int")])
-            (parseTldAndTypecheck "funk=func():Int{1234}" typeEnv) `shouldBe` Just (Type (Identifier "Int"))
+            (parseAndTypecheck parseTld "funk=func():Int{1234}" typeEnv) `shouldBe` Just (Type (Identifier "Int"))
 
 
          it "FAILS on bad typechecker input" $ do
             let typeEnv = (Gamma [(Identifier "anotherFunk", mkType "String")])
-            (parseTldAndTypecheck "anotherFunk=func():String{1234}" typeEnv) `shouldBe` Nothing
+            (parseAndTypecheck parseTld "anotherFunk=func():String{1234}" typeEnv) `shouldBe` Nothing
 
             let typeEnv = (Gamma [(Identifier "anotherFunk", mkType "String")])
-            (parseTldAndTypecheck "anotherFunk=func():String{a}" typeEnv) `shouldBe` Nothing
+            (parseAndTypecheck parseTld "anotherFunk=func():String{a}" typeEnv) `shouldBe` Nothing
 
             let typeEnv = (Gamma [(Identifier "function", mkType "String")])
-            (parseTldAndTypecheck "anotherFunk=func():String{x+y+x=5}" typeEnv) `shouldBe` Nothing
+            (parseAndTypecheck parseTld "anotherFunk=func():String{x+y+x=5}" typeEnv) `shouldBe` Nothing
