1586d93892be9818f4a7ed7e6ae014f71d6f2ca8
Michael Munje <michaelmunje@gmail.com>
2019-04-16 21:40:31 -0700

    Fix docker not testing all. Fix bug where it only evaluated initial code

--- docker/test.sh
+++ docker/test.sh
@@ -1,2 +1,2 @@
 cd ..
-docker run -ti --rm --net=host -v $(pwd):/funkadelic:rw  funkadelic bash -c "stack test --coverage"
+docker run -ti --rm --net=host -v $(pwd):/funkadelic:rw  funkadelic bash -c "cd ../funkadelic ; stack test "
