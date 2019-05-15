e322f60ad8fd014eb9efd81eb902052e00725924
Michael Munje <michaelmunje@gmail.com>
2019-05-15 14:51:51 -0700

    Add gamma as argument to the translator

--- src/Translator.hs
+++ src/Translator.hs
@@ -1,30 +1,32 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Translator where
 
 import Parser
+import Typechecker
 
-class Translate a where
-    translate :: a -> String
+class Translate a g where
+    translate :: a -> g -> String
 
-instance Translate IExp where
-    translate (IExpInt a) = (show a)
-    translate (IExpVar (Identifier a)) = a
-    translate (IExp ie1 iBinOp ie2) = translate ie1 ++ " "++ (show iBinOp) ++ " " ++ translate ie2
+instance Translate IExp Gamma where
+    translate (IExpInt a) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = (show a)
+    translate (IExpVar (Identifier a)) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = a
+    translate (IExp ie1 iBinOp ie2) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = translate ie1 (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) ++ " "++ (show iBinOp) ++ " " ++ translate ie2 (Gamma (Env l, TldMap m, TcDef td, TcImp ti))
 
-instance Translate Exp where
-    translate (ExpVariable (Identifier id)) = id
-    translate (ExpInteger a) = (show a)
-    translate (ExpString a) = (show a)
-    translate (ExpLambda e1 _ e2 _) = "function(" ++ (translate e1) ++ ") {" ++ (translate e2) ++ "}"
-    translate (ExpIExp a) = (translate a)
-    translate (ExpUnaryFOCall (Identifier id) e1) = id ++ "(" ++ (translate e1) ++ ")"
-    translate (ExpNullaryFOCall (Identifier id)) = id ++ "()"
+instance Translate Exp Gamma where
+    translate (ExpVariable (Identifier id)) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = id
+    translate (ExpInteger a) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = (show a)
+    translate (ExpString a) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = (show a)
+    translate (ExpLambda e1 _ e2 _) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = "function(" ++ (translate e1 (Gamma (Env l, TldMap m, TcDef td, TcImp ti))) ++ ") {" ++ (translate e2 (Gamma (Env l, TldMap m, TcDef td, TcImp ti))) ++ "}"
+    translate (ExpIExp a) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = (translate a (Gamma (Env l, TldMap m, TcDef td, TcImp ti)))
+    translate (ExpUnaryFOCall (Identifier id) e1) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = id ++ "(" ++ (translate e1 (Gamma (Env l, TldMap m, TcDef td, TcImp ti))) ++ ")"
+    translate (ExpNullaryFOCall (Identifier id)) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = id ++ "()"
 
-instance Translate CDef where
-    translate (NullaryConstructor (Identifier id)) = id ++ ":{}"
+instance Translate CDef Gamma where
+    translate (NullaryConstructor (Identifier id)) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = id ++ ":{}"
     -- translate (UnaryConstructor (Identifier id) _) = id ++ "()" -- NEEDS Monadic implementation to generate variable names
 
-instance Translate Tld where
-    translate (DataDef (Identifier id) [c]) = "let " ++ id ++ " = Data(function(){ " ++ (translate c) ++ "};" 
-    translate (Func (FuncDefUnary (Identifier fName) (Identifier pName) _ e1 _)) = "function " ++ fName ++ "(" ++ pName ++ ") { " ++ (translate e1) ++ " }"
-    translate (Func (FuncDefNullary (Identifier fName) e1 _)) = "function " ++ fName ++ "() { " ++ (translate e1) ++ " }"
+instance Translate Tld Gamma where
+    translate (DataDef (Identifier id) [c]) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = "let " ++ id ++ " = Data(function(){ " ++ (translate c (Gamma (Env l, TldMap m, TcDef td, TcImp ti))) ++ "};" 
+    translate (Func (FuncDefUnary (Identifier fName) (Identifier pName) _ e1 _)) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = "function " ++ fName ++ "(" ++ pName ++ ") { " ++ (translate e1 (Gamma (Env l, TldMap m, TcDef td, TcImp ti))) ++ " }"
+    translate (Func (FuncDefNullary (Identifier fName) e1 _)) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = "function " ++ fName ++ "() { " ++ (translate e1 (Gamma (Env l, TldMap m, TcDef td, TcImp ti))) ++ " }"
