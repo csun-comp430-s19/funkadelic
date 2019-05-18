11ac9a1e40d146a8beadc9eaf532fb9f680604ea
Shant Hairapetian <shant.hairapetian@metalab.csun.edu>
2019-05-18 12:34:56 -0700

    fixing translation

--- app/Main.hs
+++ app/Main.hs
@@ -16,8 +16,15 @@ main = do
     case parseResult of
         Right (tlds, exp) -> do
             gamma <- return $ snd (runState (typecheckProgram (tlds, exp)) emptyGamma)
-            putStrLn $ show $ translate exp gamma
-            where emptyGamma = (Gamma (Env [], TldMap [], TcDef [], TcImp []))
+            putStrLn $ concat (translatedTlds gamma ++ [translatedExp gamma])
+            where 
+                emptyGamma = (Gamma (Env [], TldMap [], TcDef [], TcImp []))
+                translatedTlds gamma = map ((flip translate) gamma) tlds 
+                translatedExp gamma = translate exp gamma
         Left e -> putStrLn $ show e
+        -- Right (tlds, exp) -> do
+        --     putStrLn $ concat $ map show tlds
+        --     where emptyGamma = (Gamma (Env [], TldMap [], TcDef [], TcImp []))
+        -- Left e -> putStrLn $ show e
     
 
--- src/Translator.hs
+++ src/Translator.hs
@@ -3,8 +3,8 @@
 module Translator where
 
 import Parser
-import Typechecker
 import Data.List
+import Typechecker
 
 class Translate a g where
     translate :: a -> g -> String
@@ -15,11 +15,6 @@ class TranslateTcCall a i g where
 concatNLs :: [String] -> String
 concatNLs [] = []
 concatNLs (xs:xss) = xs ++ "\n" ++ concatNLs xss
-
-
-getParams :: [Identifier] -> String
-getParams [(Identifier a)] = a
-getParams (Identifier h:tail) = h ++ ", " ++ (getParams tail)
 
 instance Translate IExp Gamma where
     translate (IExpInt a) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = (show a)
@@ -34,12 +29,6 @@ instance Translate Exp Gamma where
     translate (ExpIExp a) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = (translate a (Gamma (Env l, TldMap m, TcDef td, TcImp ti)))
     translate (ExpUnaryFOCall (Identifier id) e1) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = id ++ "(" ++ (translate e1 (Gamma (Env l, TldMap m, TcDef td, TcImp ti))) ++ ")"
     translate (ExpNullaryFOCall (Identifier id)) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = id ++ "()"
-    translate (ExpPatternMatchCall e1 _ _ pmes) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = "match " ++ (translate e1 (Gamma (Env l, TldMap m, TcDef td, TcImp ti))) ++ " { " ++ (intercalate (" ") (zipWith translate (pmes) (take (length pmes) (repeat (Gamma (Env l, TldMap m, TcDef td, TcImp ti)))))) ++ "}"
-
-instance Translate Pme Gamma where
-    translate (PatternMatchExpression (Identifier cid) [] re) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) =  cid ++ ": " ++ (translate re (Gamma (Env l, TldMap m, TcDef td, TcImp ti)))
-    translate (PatternMatchExpression (Identifier cid) ((Identifier pid):tail) re) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = cid ++ "(" ++ pid ++ ", " ++ (getParams tail) ++ "): " ++ (translate re (Gamma (Env l, TldMap m, TcDef td, TcImp ti)))
-        
 
 instance TranslateTcCall Exp Type Gamma where
     translateTcCall (TypeclassCallInt (ExpAtomInt num) (Typeclass (Identifier tcName)) (TypeclassFunc (Identifier tcFuncName))) (Type (Identifier t)) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = "_" ++ tcName ++ tcFuncName ++ t ++ "(" ++ show num ++ ")"
@@ -52,13 +41,14 @@ instance TranslateTcCall Exp Type Gamma where
 
 
 instance Translate CDef Gamma where
-    translate (NullaryConstructor (Identifier id)) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = id ++ ":{}"
-    -- translate (UnaryConstructor (Identifier id) _) = id ++ "()" -- NEEDS Monadic implementation to generate variable names
+    translate (NullaryConstructor (Identifier id)) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = "data " ++ id
+    translate (UnaryConstructor (Identifier id) (Type (Identifier t))) _  = "data " ++ id ++ "{x:" ++ t ++ "}"
 
 instance Translate Tld Gamma where
-    translate (DataDef (Identifier id) [c]) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = "let " ++ id ++ " = Data(function(){ " ++ (translate c (Gamma (Env l, TldMap m, TcDef td, TcImp ti))) ++ "};" 
+    translate (DataDef (Identifier id) c) gamma = "union " ++ id ++ " { " ++ (concat $ intersperse "," $ map (flip translate gamma) c) ++ "}" 
     translate (Func (FuncDefUnary (Identifier fName) (Identifier pName) _ e1 _)) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = "function " ++ fName ++ "(" ++ pName ++ ") { " ++ (translate e1 (Gamma (Env l, TldMap m, TcDef td, TcImp ti))) ++ " }"
-    translate (Func (FuncDefNullary (Identifier fName) e1 _)) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = "function " ++ fName ++ "() { " ++ (translate e1 (Gamma (Env l, TldMap m, TcDef td, TcImp ti))) ++ " }"
+    translate (Func (FuncDefNullary (Identifier fName) e1 _)) gamma = "function " ++ fName ++ "() { " ++ (translate e1 gamma) ++ " }"
+    translate (TypeclassDef _ _) _ = ""
     translate (TypeclassImp (Identifier tcName) imps) (Gamma (Env l, TldMap m, TcDef td, TcImp ti)) = (concatNLs (map imp2Fun imps))
         where 
-            imp2Fun (SigImp (Identifier funName) (Type (Identifier inTypeStr)) (Type (Identifier _)) (Identifier paramName) (exp)) = ("function " ++ "_" ++ tcName ++ funName ++ inTypeStr ++ "(" ++ paramName ++ ") { " ++ (translate exp (Gamma (Env l, TldMap m, TcDef td, TcImp ti))) ++ " }")
+        imp2Fun (SigImp (Identifier funName) (Type (Identifier inTypeStr)) (Type (Identifier _)) (Identifier paramName) (exp)) = ("function " ++ "_" ++ tcName ++ funName ++ inTypeStr ++ "(" ++ paramName ++ ") { " ++ (translate exp (Gamma (Env l, TldMap m, TcDef td, TcImp ti))) ++ " }")
